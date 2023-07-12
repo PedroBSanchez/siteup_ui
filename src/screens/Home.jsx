d1eb347cd4f18f2bbc9f1a567e9cf7b730453f6f
PedroBSanchez <pedro_b47@hotmail.com>
2023-07-12 20:18:04 -0300

    fix

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -5,9 +5,6 @@ import "./Home.css";
 import UserCard from "../components/UserCard";
 
 const Home = () => {
-  console.log(localStorage.getItem("userEmail"));
-  console.log(localStorage.getItem("tokenApi"));
-
   return (
     <div className="container-fluid">
       <div className="row home-background p-3">
